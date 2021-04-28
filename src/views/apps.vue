3267bf77750cdb387bb01e09b6f984134ed81d12
lcssl <css.1999@outlook.com>
2021-04-28 18:34:01 +0800

    up

--- src/views/apps.vue
+++ src/views/apps.vue
@@ -3,6 +3,7 @@
     <el-carousel :interval="4000" type="card" class="chooseApps" >
       <el-carousel-item class="my-carousel-item" v-for="(item,index) in appList" :label="item.desc" :key="index">
         <el-image fit="cover" :src="item.banner" @click="jump(item.url)"/>
+
       </el-carousel-item>
     </el-carousel>
   </div>
@@ -12,25 +13,49 @@
 
 export default {
   name: 'Apps',
+  computed:{
+    appList(){
+      if ( process.env.NODE_ENV==='production'){
+        return [
+          {
+            url:'http://www.gzkaiyu56.cn',
+            banner:'http://server.kaiyu.work:9000/test/gzkaiyu56.png',
+            desc:'官网'
+          },
+          {
+            url:'http://lms.kaiyu.work:9527',
+            banner:'http://server.kaiyu.work:9000/test/logo.png',
+            desc:'LMS管理系统'
+          },
+          {
+            url:'https://www.baidu.com',
+            banner:'http://server.kaiyu.work:9000/test/baidu.png',
+            desc:'Baidu'
+          },
+        ]
+      }else {
+        return [
+          {
+            url:'http://www.gzkaiyu56.cn',
+            banner:'http://server.kaiyu.work:9000/test/gzkaiyu56.png',
+            desc:'官网'
+          },
+          {
+            url:'http://localhost:1024',
+            banner:'http://server.kaiyu.work:9000/test/logo.png',
+            desc:'LMS管理系统'
+          },
+          {
+            url:'https://www.baidu.com',
+            banner:'http://server.kaiyu.work:9000/test/baidu.png',
+            desc:'Baidu'
+          },
+        ]
+      }
+    }
+  },
   data() {
     return {
-      appList:[
-        {
-          url:'http://www.gzkaiyu56.cn',
-          banner:'http://server.kaiyu.work:9000/test/gzkaiyu56.png',
-          desc:'官网'
-        },
-        {
-          url:'http://localhost:1024',
-          banner:'http://server.kaiyu.work:9000/test/logo.png',
-          desc:'LMS管理系统'
-        },
-        {
-          url:'https://www.baidu.com',
-          banner:'http://server.kaiyu.work:9000/test/baidu.png',
-          desc:'Baidu'
-        },
-      ]
     }
   },
   watch: {
